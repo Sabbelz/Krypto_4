d3db9e5edecd10a3234d3155119a7360395a5573
Sebastian Reichmann <46034908+Sabbelz@users.noreply.github.com>
2020-06-29 16:21:22 +0200

    linter satisfaction!

--- Task_2a.py
+++ Task_2a.py
@@ -1,23 +1,32 @@
+"""
+Aufgabe 2.a.
+"""
 from math import gcd
 
 
 def calculate_order(array):
+    """
+    Berechnung der order
+    """
     counter = 0
     for _ in range(len(array)):
         counter += 1
     return counter
 
 
-def compute_complexity(g, p):
+def compute_complexity(generator, prime):
+    """
+    Ermittlung der Komplexität
+    """
     array = set()
-    for k in range(1, p):
-        array.add((g ** k) % p)
+    for k in range(1, prime):
+        array.add((generator ** k) % prime)
     print("order: ", calculate_order(array))
     avg_order = 0
-    for j in range(1, p):
-        for i in range(1, p):
+    for j in range(1, prime):
+        for i in range(1, prime):
             avg_order += calculate_order(array) / gcd(calculate_order(array), i * j)
-    avg_order /= pow(p - 1, 2)
+    avg_order /= pow(prime - 1, 2)
     print("average order: ", avg_order)
     return avg_order
 
